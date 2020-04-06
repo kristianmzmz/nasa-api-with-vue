e907794c314cb61b7e71e8eb7dd3f33b5221e3e7
Kristian Muñoz <kristian.munoz@codurance.com>
2020-04-06 08:52:24 +0200

    Bind the results to images to be able to display them

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,6 +4,11 @@
 		<form v-on:submit.prevent="getResult(query)">
 			<input type="text" v-model="query" />
 		</form>
+    <div class="images" v-if="results">
+      <div v-for="(result, key) in results" :key="key">
+        <img :src="result.links[0].href" />
+      </div>
+    </div>
 	</div>
 </template>
 
@@ -15,14 +20,21 @@ export default {
   data() {
     return {
       query: '',
+      results: ''
     }
   }, 
   methods: {
     getResult(query) {
       axios.get('https://images-api.nasa.gov/search?media_type=image&q=' + query).then(response => {
-        console.log(response.data.collection.items)
+        this.results = response.data.collection.items
       })
     }
   }
 }
 </script>
+<style scoped>
+.images img{
+  height: 300px;
+  margin: 10px;
+} 
+</style>
